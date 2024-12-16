af2e85ae57ef28d9c0084f0ecc6a2974f3d32da4
Rafael Direito <rdireito@av.it.pt>
2024-12-16 13:56:37 +0000

    Some To Dos

--- QoDProvisioningAPI/API/src/database/crud.py
+++ QoDProvisioningAPI/API/src/database/crud.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Authors: 
-#   Eduardo Santos (eduardosantoshf@av.it.pt) - ITAv, Portugal 
-#   Rafael Direito (rdireito@av.it.pt) - ITAv, Portugal
+#   Eduardo Santos (eduardosantoshf@av.it.pt)
+#   Rafael Direito (rdireito@av.it.pt)
+# @Organization:
+#   Instituto de Telecomunicações, Aveiro (ITAv)
+#   Aveiro, Portugal
 # @Date:
 #   December 2024
 
